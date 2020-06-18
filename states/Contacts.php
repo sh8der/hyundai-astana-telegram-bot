333bde7c3040f8bbcc03af8a10f1e6d0bb374a8a
Кирилл <youxenux@gmail.com>
2020-06-19 00:20:15 +0600

    feat: Add send geolocation and photos before text

--- states/Contacts.php
+++ states/Contacts.php
@@ -5,8 +5,42 @@ use RedBeanPHP\R;
 class Contacts extends BaseState
 {
   public $state = "Contacts";
+  public $replyMessage = null;
+  public $bot = null;
+  public $img = null;
+
   public function init($bot)
   {
-    parent::init($bot);
+    $this->bot = $bot;
+    $this->replyMessage = R::getRow(
+      'SELECT `image_list`, `message` FROM `reply_dictionary` WHERE `state` = ?',
+      [$this->state]
+    );
+    $this->img = $this->replyMessage['image_list'];
+    
+    if ($this->production) {
+      $botSubFolder = "";
+      if (array_key_exists('BOT_FOLDER', getOptions()))
+        $botSubFolder = "/" . getOptions()['BOT_FOLDER'];
+      $this->img = "https://" . trim($_SERVER['HTTP_HOST']) . "{$botSubFolder}{$this->img}";
+    } else {
+      $this->img = getNgrokPublicUrl() . $this->img;
+    }
+    
+    $this->sendTyping();
+    $params = [
+      'chat_id' => $this->bot->BotWrapperCurrentChatId,
+      'photo' => $this->img,
+      'caption' => $this->replyMessage['message'],
+      'parse_mode' => 'Markdown'
+    ];
+    $this->bot->sendPhoto($params);
+    $this->sendTyping();
+    $params = [
+      'chat_id' => $this->bot->BotWrapperCurrentChatId,
+      'latitude' => 51.118802,
+      'longitude' => 71.4107971,
+    ];
+    $this->bot->sendLocation($params);
   }
 }
